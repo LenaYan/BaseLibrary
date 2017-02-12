576251e3e05ad3273f402194c96dae0b6973da8c
Rayman.t.Yan <slyyantao@gmail.com>
2017-02-12 23:10:34 +0800

    Config espresso

--- src/main/java/com/rayman/interview/lib/model/model/PriceEntity.java
+++ src/main/java/com/rayman/interview/lib/model/model/PriceEntity.java
@@ -2,12 +2,17 @@ package com.rayman.interview.lib.model.model;
 
 public class PriceEntity {
 
+    private long id = System.nanoTime();
     private String name;
     private double price;
 
     public PriceEntity(String name, double price) {
         this.name = name;
         this.price = price;
+    }
+
+    public long getId() {
+        return id;
     }
 
     public String getName() {
