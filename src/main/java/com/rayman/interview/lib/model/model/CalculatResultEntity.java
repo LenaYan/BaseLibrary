9fee7edf84321753e16fc45b58a9f3a8f0dd6ecb
Rayman.t.Yan <slyyantao@gmail.com>
2017-02-12 17:59:17 +0800

    Modify calculator result

--- src/main/java/com/rayman/interview/lib/model/model/CalculatResultEntity.java
+++ src/main/java/com/rayman/interview/lib/model/model/CalculatResultEntity.java
@@ -2,27 +2,35 @@ package com.rayman.interview.lib.model.model;
 
 public class CalculatResultEntity {
 
-    private String input;
+    private final String input;
+    private String name;
     private String result;
+    private int resultFormatRes;
 
-    public CalculatResultEntity(String input, String result) {
+    public CalculatResultEntity(String input) {
         this.input = input;
+    }
+
+    public CalculatResultEntity(String input, String name, String result, int resultFormatRes) {
+        this.input = input;
+        this.name = name;
         this.result = result;
+        this.resultFormatRes = resultFormatRes;
     }
 
     public String getInput() {
         return input;
     }
 
-    public void setInput(String input) {
-        this.input = input;
+    public String getName() {
+        return name;
     }
 
     public String getResult() {
         return result;
     }
 
-    public void setResult(String result) {
-        this.result = result;
+    public int getResultFormatRes() {
+        return resultFormatRes;
     }
 }
